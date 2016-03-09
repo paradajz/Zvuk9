48058d86b8413783cdc150c3ea2208ef83a7bedc
paradajz <2544094+paradajz@users.noreply.github.com>
2016-03-09 13:04:47 +0100

    update unique id

--- Zvuk9/eeprom/UniqueID.h
+++ Zvuk9/eeprom/UniqueID.h
@@ -2,7 +2,7 @@
 #define UNIQUEID_H_
 
 //unique ID (used to detect if EEPROM has been formatted)
-#define EEPROM_UNIQUE_ID        22
+#define EEPROM_UNIQUE_ID        23
 
 #define UNIQUE_ID_LOCATION_0    4093
 #define UNIQUE_ID_LOCATION_1    4094
