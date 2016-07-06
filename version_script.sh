3d89ecdfc9e86430368337d594626d8a783d5bba
paradajz <2544094+paradajz@users.noreply.github.com>
2016-07-06 22:14:59 +0200

    remove newfile

--- version_script.sh
+++ version_script.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+
+#count all tags with "v*.0" pattern and assign value to $major
 major=$(git tag -l "v*.0" | wc -l)
-last_major_tag=$(git for-each-ref --format='%(*creatordate:raw)%(creatordate:raw) %(refname) %(*objectname) %(objectname)' refs/tags | sort -r | awk '{ print $3 }' | sed -e 's/refs\/tags\///g' | sed '/v*.0/q' | sort -r | head -1)
-tags_since_major=$(git for-each-ref --format='%(*creatordate:raw)%(creatordate:raw) %(refname) %(*objectname) %(objectname)' refs/tags | sort -r | awk '{ print $3 }' | sed -e 's/refs\/tags\///g' | sed '/v*.0/Q' | wc -l)
+
+#list all tags descending until major tag is found, count the output and assign result to $minor
+minor=$(git for-each-ref --format='%(*creatordate:raw)%(creatordate:raw) %(refname) %(*objectname) %(objectname)' refs/tags | sort -r | awk '{ print $3 }' | sed -e 's/refs\/tags\///g' | sed '/v*.0/Q' | wc -l)
+
+#there are no major tags, count all tags instead
+if [ $minor == 0 ]
+then
+minor=$(git tag | wc -l)
+
+#if minor is still zero, revision is number of tags
+if [ $minor == 0 ]
+then
+revision=$(git rev-list --remotes | wc -l)
+else
 last_tag_commit=$(git for-each-ref --format='%(*creatordate:raw)%(creatordate:raw) %(refname) %(*objectname) %(objectname)' refs/tags | sort -r | awk '{ print $4 }' | sed -e 's/refs\/tags\///g' | head -1)
-commits_since_last_tag=$(git rev-list --remotes | sed '/'$last_tag_commit'/Q' | wc -l)
-echo "last major version: $major"
-echo "last major tag: $last_major_tag"
-echo "tags since major version: $tags_since_major"
-echo "last tag commit: $last_tag_commit"
-echo "commits since last tag: $commits_since_last_tag"
-echo "software version: $major.$tags_since_major.$commits_since_last_tag"
+revision=$(git rev-list --remotes | sed '/'$last_tag_commit'/Q' | wc -l)
+fi
+else
+last_tag_commit=$(git for-each-ref --format='%(*creatordate:raw)%(creatordate:raw) %(refname) %(*objectname) %(objectname)' refs/tags | sort -r | awk '{ print $4 }' | sed -e 's/refs\/tags\///g' | head -1)
+revision=$(git rev-list --remotes | sed '/'$last_tag_commit'/Q' | wc -l)
+fi
+
+echo "software version: $major.$minor.$revision"
+
+#output $major, $minor and $revision into separate files
 echo "$major" > "major"
-echo "$tags_since_major" > "minor"
-echo "$commits_since_last_tag" > "revision"
+echo "$minor" > "minor"
+echo "$revision" > "revision"
