547efb3b82d3d184f600b83bfb64133e0001f569
paradajz <2544094+paradajz@users.noreply.github.com>
2016-10-06 21:34:16 +0200

    hopefully fix scaling issues when using x/y min/max

--- src/Firmware/interface/pads/curves/Curves.cpp
+++ src/Firmware/interface/pads/curves/Curves.cpp
@@ -20,6 +20,12 @@ double gain(double g, double x)    {
 
 }
 
+uint8_t map_u8(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max) {
+
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+};
+
 Curves::Curves()    {
 
     //def const
@@ -43,15 +49,22 @@ void Curves::setupCurve(uint8_t curveGain, uint8_t min, uint8_t max)   {
         //normally, gain 0.5 produces linear scale
         //however, we are converting decimal numbers to int so there is slight error
         //in this case, setup curve manually
-
         for (int i=0; i<128; i++)
             scale[i] = i;
+
+        //scale range if min isn't 0 or max isn't 127
+        if (min || max != 127) {
+
+            for (int i=0; i<128; i++)
+                scale[i] = map_u8(i, 0, 127, min, max);
+
+        }
 
     }   else {
 
         //printf("Curve scaling updated. Min: %d, max: %d\n", min, max);
 
-        double curveGain_double = curveGain/10.0; //scale gain value 0-1
+        double curveGain_double = curveGain/10.0; //scale gain value to values 0.0-1.0
         uint8_t numberOfValues = max-min;
         double step, stepValue = 0.0;
 
@@ -61,9 +74,13 @@ void Curves::setupCurve(uint8_t curveGain, uint8_t min, uint8_t max)   {
         for (int i=0; i<=numberOfValues; i++)   {
 
             //make sure that curve extremes are correct
-            if (!i) scale[i] = 0;
-            else if (i == numberOfValues) scale[i] = numberOfValues;
-            else scale[i] = round(gain(curveGain_double, stepValue) * numberOfValues); //round up
+            if (!i)
+                scale[i] = 0;
+            else if (i == numberOfValues)
+                scale[i] = numberOfValues;
+            else
+                scale[i] = round(gain(curveGain_double, stepValue) * numberOfValues); //round up
+
             stepValue += step;
 
         }
@@ -78,16 +95,16 @@ void Curves::setupCurve(uint8_t curveGain, uint8_t min, uint8_t max)   {
 
             for (int i=0; i<=numberOfValues; i++)   {
 
-                if (!i) scale[i] = min;
-                else if (i == max) scale[i] = max;
-                else scale[i] = pads.map(tempArray[i], 0, numberOfValues, min, max);
+                if (!i)
+                    scale[i] = min;
+                else if (i == max)
+                    scale[i] = max;
+                else
+                    scale[i] = map_u8(tempArray[i], 0, numberOfValues, min, max);
 
             }
 
         }
-
-        //for (int i=0; i<=numberOfValues; i++)
-            //printf("%d\n", scale[i]);
 
     }
 
@@ -96,6 +113,10 @@ void Curves::setupCurve(uint8_t curveGain, uint8_t min, uint8_t max)   {
 uint8_t Curves::getCurveValue(coordinateType_t coordinate, uint8_t curveGain, uint8_t index, uint8_t min, uint8_t max)    {
 
     if (curveGain != lastGain[(uint8_t)coordinate])   {
+
+        #ifdef DEBUG
+        printf("Setting up new x/y scale\n");
+        #endif
 
         //only copy bytes from flash if received gain differs from last one
         memcpy_P(scale, (uint8_t*)pgm_read_word(&(scaleArray[curveGain-1])), 128);
@@ -114,6 +135,10 @@ uint8_t Curves::getCurveValue(coordinateType_t coordinate, uint8_t curveGain, ui
     if ((min != 0) || (max != 127))    {
 
         if ((lastMin[(uint8_t)coordinate] != min) || (lastMax[(uint8_t)coordinate] != max))   {
+
+            #ifdef DEBUG
+            printf("Setting up new x/y value\n");
+            #endif
 
             lastMin[(uint8_t)coordinate] = min;
             lastMax[(uint8_t)coordinate] = max;
