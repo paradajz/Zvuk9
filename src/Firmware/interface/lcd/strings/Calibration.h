59d30b5b818d52442c3203f37815952fdd93bcf4
paradajz <2544094+paradajz@users.noreply.github.com>
2016-10-09 11:50:22 +0200

    add new calibration strings

--- src/Firmware/interface/lcd/strings/Calibration.h
+++ src/Firmware/interface/lcd/strings/Calibration.h
@@ -1,6 +1,8 @@
 #ifndef CALIBRATION_STRINGS_H_
 #define CALIBRATION_STRINGS_H_
 
-
+const char xCalibration_string[] PROGMEM = "Scroll horizontally";
+const char yCalibration_string[] PROGMEM = "Scroll vertically";
+const char zCalibration_string[] PROGMEM = "Set max velocity";
 
 #endif
--- src/Firmware/interface/lcd/strings/Strings.h
+++ src/Firmware/interface/lcd/strings/Strings.h
@@ -11,6 +11,7 @@
 #include "PadAmount.h"
 #include "Misc.h"
 #include "Reset.h"
+#include "Calibration.h"
 
 //note: in _sizes arrays, we need to subtract 1 because of EOL char
 
