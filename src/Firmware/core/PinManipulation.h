132dda4a1635f698f2047fd2d2de0522cac6dda8
paradajz <2544094+paradajz@users.noreply.github.com>
2016-10-24 08:28:44 +0200

    add wrong y setup comments, delete incorrect/unnecessary pinmanipulation lines

--- src/Firmware/core/PinManipulation.h
+++ src/Firmware/core/PinManipulation.h
@@ -4,12 +4,7 @@
 #define DDR(x) (*(&x-1))
 
 //see register summary in datasheet for selected device
-#if defined(__AVR_ATmega64__) || defined(__AVR_ATmega128__)
-    /* on ATmega64/128 PINF is on port 0x00 and not 0x60 */
-    #define PIN(x) ( &PORTF==&(x) ? _SFR_IO8(0x00) : (*(&x - 2)) )
-#else
-    #define PIN(x) (*(&x-2))
-#endif
+#define PIN(x) (*(&x-2))
 
 #define setOutput(port, pin) ((DDR(port)) |= (1 << (pin)))
 #define setInput(port, pin) ((DDR(port)) &= ~(1 << (pin)))
