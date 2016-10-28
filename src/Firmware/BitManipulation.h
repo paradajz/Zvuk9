842c745deaea35f1a40e48d35052ce56cb2cab22
paradajz <2544094+paradajz@users.noreply.github.com>
2016-10-28 20:29:50 +0200

    pragma once

--- src/Firmware/BitManipulation.h
+++ src/Firmware/BitManipulation.h
@@ -1,10 +1,7 @@
-#ifndef BITMANIPULATION_H_
-#define BITMANIPULATION_H_
+#pragma once
 
 #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
 #define bitSet(value, bit) ((value) |= (1UL << (bit)))
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 #define invertByte(value) ((value) ^ 0xFF)
-
-#endif
