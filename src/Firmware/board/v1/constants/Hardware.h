91cc3be0b661830bdc073cde3c7826df7a7ecefb
paradajz <2544094+paradajz@users.noreply.github.com>
2017-01-28 12:02:18 +0100

    don't hardcode max number of leds - use column/row math instead

--- src/Firmware/board/v1/constants/Hardware.h
+++ src/Firmware/board/v1/constants/Hardware.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #define NUMBER_OF_ENCODERS          10
-#define NUMBER_OF_LEDS              24
 #define MAX_PADS                    9
-#define MAX_NUMBER_OF_BUTTONS       32
 #define NUMBER_OF_LED_COLUMNS       8
 #define NUMBER_OF_LED_ROWS          3
+#define MAX_NUMBER_OF_BUTTONS       32
+#define MAX_NUMBER_OF_LEDS          (NUMBER_OF_LED_COLUMNS*NUMBER_OF_LED_ROWS)
 
 //uncomment if leds use reverse logic for setting on/off state
 //#define LED_INVERT
