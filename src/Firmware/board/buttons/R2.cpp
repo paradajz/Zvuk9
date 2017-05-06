3d48b2932b683447194eecfe899635425336b90c
paradajz <2544094+paradajz@users.noreply.github.com>
2017-05-06 11:28:51 +0200

    remove unused functions

--- src/Firmware/board/buttons/R2.cpp
+++ src/Firmware/board/buttons/R2.cpp
@@ -1,13 +1,6 @@
 #include "../Board.h"
 
-bool buttonsProcessed;
-
 volatile uint8_t    buttonDebounceCounter[MAX_NUMBER_OF_BUTTONS];
-
-bool Board::buttonDataAvailable()
-{
-    return true;
-}
 
 uint8_t Board::getButtonState(uint8_t buttonIndex)
 {
