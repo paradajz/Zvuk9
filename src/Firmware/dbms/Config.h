2eec1eca79aa7df577b16ba342565d64e81a91a9
paradajz <2544094+paradajz@users.noreply.github.com>
2017-05-08 21:08:23 +0200

    remove lufa, dbms, midi and sysex

--- src/Firmware/dbms/Config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ENABLE_ASYNC_UPDATE
-#define AUTO_INCREMENT              255
-#define MAX_SECTIONS                7
-#define MAX_BLOCKS                  7
-#define EEPROM_UPDATE_BUFFER_SIZE   20
