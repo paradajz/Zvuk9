78bc59500b0b0e0a6e66002b2d92357856effcb2
paradajz <2544094+paradajz@users.noreply.github.com>
2017-06-28 20:54:03 +0200

    define some sanity checks in separate header

---
+++ src/Firmware/interface/pads/Sanity.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PAD_CHECK(pad)              (((pad < 0) || (pad >= NUMBER_OF_PADS)) ? 0 : 1)
+#define NOTE_ASSIGN_CHECK(note)     (((note < 0) || (note >= NOTES_PER_PAD)) ? 0 : 1)
