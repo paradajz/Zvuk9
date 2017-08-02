2e904f707b30216e6365b504f6aa857dee0a2e81
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:31:08 +0200

    group pad strings to dedicated file

---
+++ src/Firmware/interface/lcd/strings/Pads.h
@@ -0,0 +1,6 @@
+#pragma once
+
+const char padAmountSingle_string[] PROGMEM = "pad";
+const char padPress_string[] PROGMEM = "pad: ";
+const char padAmountAll_string[] PROGMEM = "all pads";
+const char padEditMode_string[] PROGMEM = "Pad editing";
