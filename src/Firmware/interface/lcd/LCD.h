d7178de395cc7561ceffb0470b83ebe6d394af29
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:42:03 +0200

    naming conventions, misc

--- src/Firmware/interface/lcd/LCD.h
+++ src/Firmware/interface/lcd/LCD.h
@@ -5,7 +5,7 @@
 #include "../pads/Pads.h"
 #include "Config.h"
 #include "DataTypes.h"
-
+#include "Layout.h"
 #include "Macros.h"
 #include "strings/Strings.h"
 
@@ -19,25 +19,28 @@ class LCD
     LCD();
     void init();
     bool update();
+
     void setDirectWriteState(bool state);
+
     void displayWelcomeMessage();
-    void displayProgramAndScale(uint8_t program, uint8_t scale);
-    void clearLine(uint8_t row);
-    void clearLines(uint8_t startIndex, uint8_t endIndex = 0);
 
-    void displayVelocity(uint8_t velocity);
-    void displayAftertouch(uint8_t afterTouch);
-    void displayXYposition(uint8_t position, padCoordinate_t type);
-    void displayXYcc(uint8_t ccXY, padCoordinate_t type);
+    void setupHomeScreen();
 
-    void clearVelocity();
-    void clearAftertouch();
-    void clearXYposition(padCoordinate_t type);
-    void clearXYcc(padCoordinate_t type);
-    void clearPadEditMode();
-    void clearPad();
-    void clearMIDIchannel();
-    void clearPadData();
+    void displayProgramInfo(uint8_t program, uint8_t scale, note_t tonic, int8_t scaleShift);
+    void displayPad(uint8_t pad = 255);
+    void displayActivePadNotes(bool showNotes = true);
+    void displayVelocity(uint8_t velocity = 255);
+    void displayAftertouch(uint8_t aftertouch = 255);
+    void displayXYcc(padCoordinate_t type, uint8_t cc = 255);
+    void displayXYposition(padCoordinate_t type, uint8_t position = 255);
+    void displayMIDIchannel(uint8_t channel = 255);
+
+    void setupPadEditScreen(uint8_t pad, uint8_t octave, bool forceRefresh = false);
+
+    void clearPadPressData();
+
+    void clearAll();
+    void clearLine(uint8_t row, bool writeToDisplay = true);
 
     void displayFirmwareUpdated();
 
@@ -47,22 +50,16 @@ class LCD
     void displayCClimitChange(padCoordinate_t coordinate, limitType_t type, uint8_t ccValue);
     void displayCCchange(padCoordinate_t type, uint8_t ccValue);
     void displayMIDIchannelChange();
-    void displayActivePadNotes();
-    void displayActiveOctave();
     void displayNoteChange(changeOutput_t result, noteChangeType_t type, int8_t value);
     void displayEditModeNotAllowed(padEditModeResult_t errorType);
     void displayNoNotesError();
-    void displayPadEditMode();
     void displayOutOfRange();
     void displayMaxNotesSet();
     void displayPadReleaseError(padReleaseError_t error);
     void displayPadEditChangeParametersError();
-    void displayPad();
-    void displayMIDIchannel();
     void displayFactoryResetConfirm();
     void displayFactoryResetStart();
     void displayFactoryResetEnd();
-    void displayNoteShiftLevel(int8_t level);
     void displayNoteUpDown(bool state, int8_t shiftLevel = 0);
 
     void displayScrollCalibrationStatus(bool status);
@@ -92,14 +89,13 @@ class LCD
     }
 
     protected:
-    void updateDisplay(uint8_t row, lcdTextType type, uint8_t startIndex, bool overwrite, uint8_t size);
+    void updateDisplay(uint8_t row, lcdTextType type, uint8_t startIndex, uint8_t size);
 
     private:
     messageStatus_t getMessageStatus();
-    void displayText(uint8_t row, const char *text, uint8_t startIndex, bool overwrite);
-    void displayMessage(uint8_t row, const char *message);
+    void displayText(uint8_t row, const char *text, uint8_t startIndex);
+    void displayMessage(uint8_t row, const char *message, uint8_t startIndex);
     void displayPadAmount(uint8_t padNumber);
-    void setupLCDlayout();
     void removeMessage();
 
     uint32_t messageDisplayTime;
@@ -116,33 +112,6 @@ class LCD
     uint32_t charChange[LCD_HEIGHT];
 
     int32_t messageTime;
-
-    typedef struct
-    {
-        uint8_t row;
-        uint8_t startIndex;
-    } lcdElement_t;
-
-    typedef struct
-    {
-        lcdElement_t ccx;
-        lcdElement_t ccy;
-        lcdElement_t xpos;
-        lcdElement_t ypos;
-        lcdElement_t velocity;
-        lcdElement_t aftertouch;
-        lcdElement_t padNumber;
-        lcdElement_t midiChannel;
-        lcdElement_t programAndScale;
-        lcdElement_t notes;
-        lcdElement_t activeOctave;
-        lcdElement_t messageText1;
-        lcdElement_t messageText2;
-        lcdElement_t padCalibration;
-        lcdElement_t noteShiftLevel;
-    } lcdElements_t;
-
-    lcdElements_t lcdElements;
 };
 
 extern LCD display;
