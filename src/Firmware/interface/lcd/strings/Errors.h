e71f9b580629318fae111bf0424682f522746dcc
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:38:11 +0200

    add exclamation marks

--- src/Firmware/interface/lcd/strings/Errors.h
+++ src/Firmware/interface/lcd/strings/Errors.h
@@ -3,19 +3,19 @@
 const char outOfRange_string[] PROGMEM = "Out of range";
 
 const char editModeEnterError0_string[] PROGMEM = "Select user scale";
-const char editModeEnterError1_string[] PROGMEM = "to edit pad notes";
+const char editModeEnterError1_string[] PROGMEM = "to edit pad notes!";
 
 const char relasePad_string[] PROGMEM = "Release all pads to";
-const char changeParameters_string[] PROGMEM = "change parameters";
-const char changeProgram_string[] PROGMEM = "change program";
-const char changePreset_string[] PROGMEM = "change scale";
-const char changeCurve_string[] PROGMEM = "change curve";
-const char changeCC_string[] PROGMEM = "change CC";
-const char changeCClimit_string[] PROGMEM = "change CC limit";
-const char enterPadEditMode_string[] PROGMEM = "enter pad edit mode";
-const char calibrationMode_string[] PROGMEM = "enter calibration";
+const char changeParameters_string[] PROGMEM = "change parameters!";
+const char changeProgram_string[] PROGMEM = "change program!";
+const char changePreset_string[] PROGMEM = "change scale!";
+const char changeCurve_string[] PROGMEM = "change curve!";
+const char changeCC_string[] PROGMEM = "change CC!";
+const char changeCClimit_string[] PROGMEM = "change CC limit!";
+const char enterPadEditMode_string[] PROGMEM = "enter pad edit mode!";
+const char calibrationMode_string[] PROGMEM = "enter calibration!";
 
 const char exitPadMode_string[] PROGMEM = "Exit pad edit mode to";
 
 const char maxNotesSet0_string[] PROGMEM = "Maximum number of";
-const char maxNotesSet1_string[] PROGMEM = "notes set";
+const char maxNotesSet1_string[] PROGMEM = "notes set!";
