8c34ec3a48231ec12d6d19bab026e0d8c3024697
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:37:48 +0200

    string tweaks

--- src/Firmware/interface/lcd/strings/Misc.h
+++ src/Firmware/interface/lcd/strings/Misc.h
@@ -1,12 +1,7 @@
 #pragma once
 
-const char emptyLine_string[] PROGMEM = "                    ";
 const char program_string[] PROGMEM = "P";
-const char aftertouch_string[] PROGMEM = "at";
 const char welcome_string[] PROGMEM = "Welcome!";
 const char midiChannelChange_string[] PROGMEM = "MIDI channel ";
-const char editingPad_string[] PROGMEM = "Editing pad";
-const char aftertouchClear_string[] PROGMEM = "     ";
-const char midiChannelClear_string[] PROGMEM = "    ";
-const char midiChannel_string[] PROGMEM = "ch";
+const char midiChannel_string[] PROGMEM = "MIDI ch: ";
 const char firmware_updated[] PROGMEM = "Firmware updated!";
