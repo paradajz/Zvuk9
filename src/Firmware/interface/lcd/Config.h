184c3fedc47786fbbf210cc47618a99bcd026d14
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:42:44 +0200

    remove ROW_SPACING (unused), set correct LCD_WIDTH

--- src/Firmware/interface/lcd/Config.h
+++ src/Firmware/interface/lcd/Config.h
@@ -10,8 +10,7 @@
 
 #define LCD_PAD_DATA_CLEAR_DELAY            300
 
-#define LCD_WIDTH                           30
+#define LCD_WIDTH                           32
 #define LCD_HEIGHT                          5
-#define ROW_SPACING                         15
 
 #define INFINITE_MESSAGE_TIME               -1
