38d46e09085aee5e46abb387bfd5ff9204f928d8
paradajz <2544094+paradajz@users.noreply.github.com>
2017-08-02 19:33:14 +0200

    define some global midi settings in dedicated file

---
+++ src/Firmware/interface/MIDIconf.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define MIDI_SETTING_TRANSPORT_CC_PLAY              125
+#define MIDI_SETTING_TRANSPORT_CC_STOP              126
+#define MIDI_SETTING_TRANSPORT_CC_RECORD            127
