e1eb5dcb2450f61a3784e59b22ac45a48e3d1f0c
paradajz <2544094+paradajz@users.noreply.github.com>
2018-04-11 11:02:50 +0200

    cleanups

--- src/application/board/Variables.h
+++ src/application/board/Variables.h
@@ -29,11 +29,7 @@
 
 extern volatile uint8_t     digitalInBuffer[NUMBER_OF_BUTTON_COLUMNS];
 extern volatile uint8_t     activeInColumn;
-
 extern volatile int8_t      activeLEDcolumn;
-extern uint8_t              encoderState[MAX_NUMBER_OF_ENCODERS];
-extern volatile int8_t      encPulses[MAX_NUMBER_OF_ENCODERS];
-extern int8_t               encPulses_x4[MAX_NUMBER_OF_ENCODERS];
 extern volatile uint16_t    padPressed;
 extern uint8_t              padReadingIndex;
 extern volatile int8_t      transitionCounter[MAX_NUMBER_OF_LEDS];
