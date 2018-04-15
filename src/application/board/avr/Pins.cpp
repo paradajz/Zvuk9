c615b1c7d7749c2b7bffc9affd511bf72a995bcc
paradajz <2544094+paradajz@users.noreply.github.com>
2018-04-15 20:22:35 +0200

    init unused pins

--- src/application/board/avr/Pins.cpp
+++ src/application/board/avr/Pins.cpp
@@ -79,4 +79,60 @@ void Board::initPins()
     setOutput(DECODER_IN_A0_PORT, DECODER_IN_A0_PIN);
     setOutput(DECODER_IN_A1_PORT, DECODER_IN_A1_PIN);
     setOutput(DECODER_IN_A2_PORT, DECODER_IN_A2_PIN);
+
+    setInput(BTLDR_BUTTON_PORT, BTLDR_BUTTON_PIN);
+    setHigh(BTLDR_BUTTON_PORT, BTLDR_BUTTON_PIN);
+
+    //init unused pins - output, low
+    setOutput(PORTA, 0);
+    setOutput(PORTA, 1);
+    setOutput(PORTA, 2);
+    setOutput(PORTA, 6);
+    setOutput(PORTA, 7);
+
+    setLow(PORTA, 0);
+    setLow(PORTA, 1);
+    setLow(PORTA, 2);
+    setLow(PORTA, 6);
+    setLow(PORTA, 7);
+
+    setOutput(PORTB, 0);
+    setOutput(PORTB, 3);
+    setOutput(PORTB, 7);
+
+    setLow(PORTB, 0);
+    setLow(PORTB, 3);
+    setLow(PORTB, 7);
+
+    setOutput(PORTC, 6);
+    setOutput(PORTC, 7);
+
+    setLow(PORTC, 6);
+    setLow(PORTC, 7);
+
+    setOutput(PORTD, 0);
+    setOutput(PORTD, 1);
+    setOutput(PORTD, 2);
+    setOutput(PORTD, 4);
+
+    setLow(PORTD, 0);
+    setLow(PORTD, 1);
+    setLow(PORTD, 2);
+    setLow(PORTD, 4);
+
+    setOutput(PORTE, 0);
+    setOutput(PORTE, 1);
+    setOutput(PORTE, 3);
+    setOutput(PORTE, 4);
+    setOutput(PORTE, 5);
+    setOutput(PORTE, 6);
+    setOutput(PORTE, 7);
+
+    setLow(PORTE, 0);
+    setLow(PORTE, 1);
+    setLow(PORTE, 3);
+    setLow(PORTE, 4);
+    setLow(PORTE, 5);
+    setLow(PORTE, 6);
+    setLow(PORTE, 7);
 }
