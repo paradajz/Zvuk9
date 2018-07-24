bb66aea36bcef63d711a97e85a6865b8ae20ce89
paradajz <2544094+paradajz@users.noreply.github.com>
2018-07-24 21:04:27 +0200

    use much higher default value for upper pressure

--- src/application/database/Layout.h
+++ src/application/database/Layout.h
@@ -107,7 +107,7 @@ static dbSection_t padCalibrationSections[PAD_CALIBRATION_SECTIONS] =
         .numberOfParameters = NUMBER_OF_PADS,
         .parameterType = WORD_PARAMETER,
         .preserveOnPartialReset = true,
-        .defaultValue = 400,
+        .defaultValue = 1300,
         .autoIncrement = false,
         .address = 0
     },
